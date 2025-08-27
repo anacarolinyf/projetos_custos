f228ec0ca5f223a83aee4d56accb6c96bf077307
anacarolinyf <carolinyfortesana@gmail.com>
2025-08-27 16:31:43 -0300

    ajustes

--- primeiro-teste/src/App.js
+++ primeiro-teste/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Evento from './components/Evento';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
-     <h1> MEU PRIMEIRO REACT</h1>
+     <h1>Testando Eventos</h1>
+     <Evento numero="1"/>
+     <Evento numero="2"/>
+     <Formulario/>
+
     </div>
   );
 }
