d177f2f26f7fd66321590a7e179ec364709a8c76
anacarolinyf <161509042+anacarolinyf@users.noreply.github.com>
2025-09-03 16:30:31 -0300

    Add files via upload

---
+++ Frases.js
@@ -0,0 +1,11 @@
+import styles from './Frase.module.css'
+
+function Frase(){
+    return(
+        <div>
+            <p className= {styles.paragrafo}>Este é um componente de frase</p>
+        </div>
+    )
+}
+
+export default Frase
